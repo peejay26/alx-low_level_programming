744ec12b9eabd3b3336e918e5e7b8b9dce173ac6
Adepeju <fadiranadepeju@gmail.com>
2022-09-16 15:56:21 -0700

    0-isupper.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ 
+* main - print putchar to output
+*
+*  Return: Always 0 (Success)
+*/
+int main (void)
+{
+	_putchar('_');
+	_putchar('_');
+	_putchar('_');
+	_putchar('_');
+	_putchar('_');
+	_putchar('_');
+	_putchar('_');
+	_putchar(' _' client_loop:
+	_putchar('\n');
+-P
+-U
+-T
+-C
+-H
+-E
+_R 
+
+$  
+
